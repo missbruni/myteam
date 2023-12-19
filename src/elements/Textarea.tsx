3c8ca5f31011285b90c2eea243630be66fb77e34
bruna <bruna.lima@guestline.com>
2023-12-19 22:36:33 +0000

    start on home section 1 on mobile

--- src/elements/Textarea.tsx
+++ src/elements/Textarea.tsx
@@ -12,11 +12,11 @@ const Textarea: React.FC<TextareaProps> = ({ required = false, label, errorText 
       <textarea
         required={required}
         placeholder={label}
-        className="peer h-full w-full border-b border-white bg-transparent pt-2 pb-1.5 pl-4 outline outline-0 transition-all focus:border-emerald-300 text-white placeholder:text-gray-500 focus:outline-0 disabled:border-gray-500 invalid:[&:not(:placeholder-shown)]:border-rose-400"
+        className="peer h-full w-full border-b border-white bg-transparent pt-2 pb-1.5 pl-4 outline outline-0 transition-all focus:border-emerald-300 text-white placeholder:text-gray-500 focus:outline-0 disabled:border-gray-500 invalid:[&:not(:placeholder-shown)]:border-[#f67e7e]"
       />
       <span className="mt-2 after:content[' '] text-gray-400 pointer-events-none absolute left-0 -top-2.5 flex h-full w-full select-none transition-all after:absolute after:-bottom-[2px] after:block after:w-full after:scale-x-0 after:border-b after:border-emerald-300  after:transition-transform after:duration-300 peer-placeholder-shown:text-gray-500 peer-focus:text-white peer-focus:after:scale-x-100 peer-focus:after:border-emerald-300 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-gray-500" />
       {errorText && (
-        <p className="mt-1 invisible text-rose-400 text-[12px] italic peer-invalid:visible">
+        <p className="mt-1 invisible text-[#f67e7e] text-[12px] italic peer-invalid:visible">
           {errorText}
         </p>
       )}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
+import PatternSvg1 from '../assets/bg-pattern-home-1.svg?react';
+import PatternSvg2 from '../assets/bg-pattern-home-2.svg?react';
+import PatternSvg3 from '../assets/bg-pattern-home-3.svg?react';
+import PatternSvg4 from '../assets/bg-pattern-home-4-about-3.svg?react';
+import PatternSvg5 from '../assets/bg-pattern-home-5.svg?react';
+import PatternSvg6 from '../assets/bg-pattern-home-6-about-5.svg?react';
+
 type HomePageProps = {};
 const HomePage: React.FC<HomePageProps> = () => {
   return (
-    <div className="w-full flex flex-col justify-center sm:justify-start text-center sm:text-left">
-      <div className="flex flex-1 items-center p-6 md:p-12 bg-cyan-950">Find the Best Talent</div>
-      <div className="flex flex-1 items-center p-6 md:p-12 bg-teal-950">
+    <div className="w-full flex flex-col justify-center sm:justify-start">
+      <div className="flex flex-1 bg-midnight-green flex-col md:flex-row">
+        <div className="w-full flex-1 flex flex-col justify-center md:justify-start items-center text-white gap-y-6 px-4 py-24">
+          <p className="text-[40px] text-center font-bold leading-[45px]">
+            Find the best <span className="text-[#f67e7e] leading-10">talent</span>
+          </p>
+          <p className="text-center leading-8 px-6">
+            Finding the right people and building high performing teams can be hard. Most companies
+            aren’t tapping into the abundance of global talent. We’re about to change that.
+          </p>
+        </div>
+        <PatternSvg2 className="w-[375px] md:hidden self-center" />
+      </div>
+      <div className="w-full flex-1 flex justify-center md:justify-start items-center py-12 text-white bg-teal-950">
         Build and Manage distributed Teams
       </div>
-      <div className="flex flex-1 items-center p-6 md:p-12 bg-cyan-950">
+      <div className="w-full flex-1 flex justify-center md:justify-start items-center py-12 text-white bg-cyan-950">
         Delivering real results
       </div>
-      <div className="flex flex-1 items-center p-6 md:p-12 bg-rose-400">Ready to get started</div>
+      <div className="w-full flex-1 flex justify-center md:justify-start items-center py-12 bg-[#f67e7e]">
+        Ready to get started
+      </div>
     </div>
   );
 };
